d5775949f24d48de6b56604c305c7cafb39fe87e
User <user@example.com>
2025-04-12 21:52:00 +0800

    log

--- src/main/java/com/bgt/billback/BillBackApplication.java
+++ src/main/java/com/bgt/billback/BillBackApplication.java
@@ -1,6 +1,8 @@
 package com.bgt.billback;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -9,8 +11,11 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @MapperScan("com.bgt.billback.mapper")
 public class BillBackApplication {
+    private static final Logger log = LoggerFactory.getLogger(BillBackApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(BillBackApplication.class, args);
+        log.info("標簽記賬后端程序啓動成功。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。");
     }
 
     @Bean
